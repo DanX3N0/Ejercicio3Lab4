ba87e9bb2ab52feed65c72a61c5582a14c799224
DanX3N0 <danieldv283@gmail.com>
2023-04-23 10:39:54 -0600

    feat(common): solution

---
+++ PilaEstatica.java
@@ -0,0 +1,57 @@
+public class PilaEstatica {
+   
+   public Integer tamañoPorDefecto = 5;
+   public Integer[] pilaArreglo;  
+   public Integer tope = -1;
+   
+   //CONSTRUCTOR
+   public PilaEstatica(){
+      pilaArreglo = new Integer[this.tamañoPorDefecto];
+   }
+   //SOBRECARGA DEL CONSTRUCTOR
+   public PilaEstatica(Integer nuevoTamaño){
+      pilaArreglo = new Integer[nuevoTamaño];
+   }
+
+   public void push(Integer elemento){
+      tope = tope + 1;//tope++;
+      pilaArreglo[tope] = elemento;
+   }
+
+   public Integer pop(){
+      Integer variableRetorno = pilaArreglo[tope];
+      tope = tope - 1;
+      return variableRetorno;
+      //return pilaArreglo[tope--];
+   } 
+
+   public boolean empty(){
+      if(tope == - 1)
+         return true;
+      return false;
+   }
+
+   public boolean full(){
+      if(tope == pilaArreglo.length - 1)
+         return true;
+      return false;
+   }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
---
+++ main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+public class main{
+   public static void main(String[] args) {
+
+      Scanner scan = new Scanner(System.in);
+      
+      Integer n = Integer.parseInt(scan.nextLine());
+      Integer m = Integer.parseInt(scan.nextLine());
+      
+      
+      PilaEstatica pilaA = new PilaEstatica(n);
+      PilaEstatica pilaB = new PilaEstatica(m);
+      PilaEstatica pilaC; 
+      if(n > m){
+         pilaC = new PilaEstatica(n);
+      }else{
+          pilaC = new PilaEstatica(m);
+      }
+      
+      System.out.println("Ingrese los datos de la pila A:");
+      
+      while(!pilaA.full()){
+         pilaA.push(Integer.parseInt(scan.nextLine()));
+      }
+
+      System.out.println("Ingrese los datos de la pila B:");
+      while(!pilaB.full()){
+         pilaB.push(Integer.parseInt(scan.nextLine()));
+      }
+
+      
+
+      if(n > m){
+         while(!pilaA.empty()){
+            Integer elemento1 = pilaA.pop();
+            Integer elemento2 = 0;
+            if(!pilaB.empty()){
+               elemento2 = pilaB.pop();
+            }
+            Integer suma = elemento1 + elemento2;
+            pilaC.push(suma);
+         }
+      }else{
+         while(!pilaB.empty()){
+            Integer elemento1 = pilaB.pop();
+            Integer elemento2 = 0;
+            if(!pilaA.empty()){
+               elemento2 = pilaA.pop();
+            }
+            Integer suma = elemento1 + elemento2;
+            pilaC.push(suma);
+         }
+      }
+
+      System.out.println("IMpresion pila C:");
+      
+      while(!pilaC.empty()){
+         System.out.println(pilaC.pop());
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+   }
+
+}
